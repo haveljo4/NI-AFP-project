59b65af3d59758f30c5bff053d956c4710765daa
Josef Havelka <josef.havelka@rapidaddition.com>
2023-05-28 23:08:20 +0200

    prepared script for installing autoconf

---
+++ .github/InstallDeps.sh
@@ -0,0 +1,10 @@
+echo "Y" | pacman -Syu
+sleep 1
+echo "Y" | pacman -S wget
+echo "Y" | pacman -S tar
+echo "Y" | pacman -S unzip
+echo "Y" | pacman -S zip
+echo "Y" | pacman -S man
+echo "Y" | pacman -S autoconf
+echo "Y" | pacman -S make
+echo "Y" | pacman -S automake
