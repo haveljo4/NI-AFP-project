116ac19e4674614f40eab1e3dcb2147204d7eb98
Josef Havelka <josef.havelka@rapidaddition.com>
2023-05-28 23:27:22 +0200

    logging the output of the mintty terminal

--- .github/InstallDeps.sh
+++ .github/InstallDeps.sh
@@ -1,10 +1,10 @@
-echo "Y" | pacman -Syu
+echo "Y" | pacman -Syu >> mintty_output.txt
 sleep 1
-echo "Y" | pacman -S wget
-echo "Y" | pacman -S tar
-echo "Y" | pacman -S unzip
-echo "Y" | pacman -S zip
-echo "Y" | pacman -S man
-echo "Y" | pacman -S autoconf
-echo "Y" | pacman -S make
-echo "Y" | pacman -S automake
+echo "Y" | pacman -S wget >> mintty_output.txt
+echo "Y" | pacman -S tar >> mintty_output.txt
+echo "Y" | pacman -S unzip >> mintty_output.txt
+echo "Y" | pacman -S zip >> mintty_output.txt
+echo "Y" | pacman -S man >> mintty_output.txt
+echo "Y" | pacman -S autoconf >> mintty_output.txt
+echo "Y" | pacman -S make >> mintty_output.txt
+echo "Y" | pacman -S automake >> mintty_output.txt
