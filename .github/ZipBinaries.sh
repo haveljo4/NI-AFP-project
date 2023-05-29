5ea69b7b5c1fb68e9fca5f706ba482b4f011ee36
Josef Havelka <josef.havelka@rapidaddition.com>
2023-05-29 23:57:49 +0200

    added installation and rest of the pipeline

--- .github/ZipBinaries.sh
+++ .github/ZipBinaries.sh
@@ -4,3 +4,4 @@ cp `dirname $(which ghc)`/../mingw/bin/libgcc_s_seh-1.dll ./tmp  &>> mintty_outp
 cp `dirname $(which ghc)`/../mingw/bin/libwinpthread-1.dll ./tmp  &>> mintty_output.txt
 cp -r ./.stack-work/dist/*/build/Chess-tool-exe/Chess-tool-exe.exe ./tmp  &>> mintty_output.txt
 zip tmp.zip ./tmp/* &>> mintty_output.txt
+exit
