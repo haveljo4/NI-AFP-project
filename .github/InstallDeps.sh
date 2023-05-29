b6d06122e7017126afd1060dd22fc371270c1dcd
Josef Havelka <josef.havelka@rapidaddition.com>
2023-05-29 16:29:37 +0200

    caching stack, trying to run mintty

--- .github/InstallDeps.sh
+++ .github/InstallDeps.sh
@@ -10,8 +10,8 @@ echo "Y" | pacman -S zip >> mintty_output.txt
 echo "Y" | pacman -S man >> mintty_output.txt
 wget http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz >> mintty_output.txt
 tar xvfvz autoconf-2.69.tar.gz >> mintty_output.txt
-cd autoconf-2.69 >> mintty_output.txt
-./configure >> mintty_output.txt
-make >> mintty_output.txt
-make install >> mintty_output.txt
+cd autoconf-2.69 >> ../mintty_output.txt
+./configure >> ../mintty_output.txt
+make >> ../mintty_output.txt
+make install >> ../mintty_output.txt
 sleep 120
