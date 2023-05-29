eaf1dfb2284c448e0ad91a2ad81a7d381f44dfe1
Josef Havelka <josef.havelka@rapidaddition.com>
2023-05-29 20:19:02 +0200

    added logging everywhere

--- .github/InstallDeps.sh
+++ .github/InstallDeps.sh
@@ -2,11 +2,17 @@ echo "before -Syy" >> mintty_output.txt
 yes | pacman -Syy >> mintty_output.txt
 echo "after -Syy" >> mintty_output.txt
 yes | pacman -Syu >> mintty_output.txt
+echo "after -Syu" >> mintty_output.txt
 yes | pacman -S wget >> mintty_output.txt
+echo "after pacman wget" >> mintty_output.txt
 yes | pacman -S tar >> mintty_output.txt
+echo "after pacman tar" >> mintty_output.txt
 yes | pacman -S unzip >> mintty_output.txt
+echo "after pacman unzip" >> mintty_output.txt
 yes | pacman -S zip >> mintty_output.txt
+echo "after pacman zip" >> mintty_output.txt
 yes | pacman -S man >> mintty_output.txt
+echo "after pacman man" >> mintty_output.txt
 echo "after pacman" >> mintty_output.txt
 yes | wget http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz >> mintty_output.txt
 yes | tar xvfvz autoconf-2.69.tar.gz >> mintty_output.txt
@@ -18,3 +24,4 @@ echo "running make" >> mintty_output.txt
 yes | make install >> ../mintty_output.txt
 sleep 10
 exit
+
