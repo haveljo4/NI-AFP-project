af9c898cc708d8e2dc457d64c11e7092e398d091
Josef Havelka <josef.havelka@rapidaddition.com>
2023-05-29 18:51:41 +0200

    added more prints

--- .github/InstallDeps.sh
+++ .github/InstallDeps.sh
@@ -5,10 +5,13 @@ echo "Y" | pacman -S tar >> mintty_output.txt
 echo "Y" | pacman -S unzip >> mintty_output.txt
 echo "Y" | pacman -S zip >> mintty_output.txt
 echo "Y" | pacman -S man >> mintty_output.txt
+echo "after pacman" >> mintty_output.txt
 wget http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz >> mintty_output.txt
 tar xvfvz autoconf-2.69.tar.gz >> mintty_output.txt
 cd autoconf-2.69 >> ../mintty_output.txt
+echo "running configure" >> mintty_output.txt
 ./configure >> ../mintty_output.txt
 make >> ../mintty_output.txt
+echo "running make" >> mintty_output.txt
 make install >> ../mintty_output.txt
 sleep 120
