310e0eddc7d827303804042bd072c52bb3c2c698
Josef Havelka <josef.havelka@rapidaddition.com>
2023-05-29 21:27:48 +0200

    trying install deps with 1 line

--- .github/InstallDeps.sh
+++ .github/InstallDeps.sh
@@ -1,27 +1,4 @@
-echo "before -Syy" >> mintty_output.txt
-yes | pacman -Syy >> mintty_output.txt
-echo "after -Syy" >> mintty_output.txt
-yes | pacman -Syu >> mintty_output.txt
-echo "after -Syu" >> mintty_output.txt
-yes | pacman -S wget >> mintty_output.txt
-echo "after pacman wget" >> mintty_output.txt
-yes | pacman -S tar >> mintty_output.txt
-echo "after pacman tar" >> mintty_output.txt
-yes | pacman -S unzip >> mintty_output.txt
-echo "after pacman unzip" >> mintty_output.txt
-yes | pacman -S zip >> mintty_output.txt
-echo "after pacman zip" >> mintty_output.txt
-yes | pacman -S man >> mintty_output.txt
-echo "after pacman man" >> mintty_output.txt
-echo "after pacman" >> mintty_output.txt
-yes | wget http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz >> mintty_output.txt
-yes | tar xvfvz autoconf-2.69.tar.gz >> mintty_output.txt
-yes | cd autoconf-2.69 >> ../mintty_output.txt
-echo "running configure" >> mintty_output.txt
-yes | ./configure >> ../mintty_output.txt
-yes | make >> ../mintty_output.txt
-echo "running make" >> mintty_output.txt
-yes | make install >> ../mintty_output.txt
-sleep 10
-exit
+pacman -Syu >> mintty_output.txt ; pacman -S --noconfirm git wget tar man gzip autoconf automake make libtool patch unzip xz bison flex pkg-config >> mintty_output.txt ;
+echo "pacman" >> mintty_output.txt
+
 
