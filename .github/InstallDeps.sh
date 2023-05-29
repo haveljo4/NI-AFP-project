81959e2b46ff5eee89ba968028b7ab1bbf010a63
Josef Havelka <josef.havelka@rapidaddition.com>
2023-05-29 21:38:56 +0200

    redirecting std and err to the same file

--- .github/InstallDeps.sh
+++ .github/InstallDeps.sh
@@ -1,4 +1,4 @@
-pacman -Syu >> mintty_output.txt ; pacman -S --noconfirm git wget tar man gzip autoconf automake make libtool patch unzip xz bison flex pkg-config >> mintty_output.txt ;
-echo "pacman" >> mintty_output.txt
+pacman -Syu &>> mintty_output.txt ; pacman -S --noconfirm git wget tar man gzip autoconf automake make libtool patch unzip xz bison flex pkg-config &>> mintty_output.txt ;
+echo "pacman" &>> mintty_output.txt
 
 
