1fb097234a185b738a90c0cee703bf194e766d3e
Josef Havelka <josef.havelka@rapidaddition.com>
2023-05-30 16:12:31 +0200

    renamed the tmp folder to chess-tool-exe

--- .github/ZipBinaries.sh
+++ .github/ZipBinaries.sh
@@ -1,7 +1,7 @@
-mkdir ./tmp  >> mintty_output.txt
-cp `dirname $(which ghc)`/../mingw/bin/libstdc++-6.dll ./tmp  &>> mintty_output.txt
-cp `dirname $(which ghc)`/../mingw/bin/libgcc_s_seh-1.dll ./tmp  &>> mintty_output.txt
-cp `dirname $(which ghc)`/../mingw/bin/libwinpthread-1.dll ./tmp  &>> mintty_output.txt
-cp -r ./.stack-work/dist/*/build/Chess-tool-exe/Chess-tool-exe.exe ./tmp  &>> mintty_output.txt
-zip chess-tool-exe.zip ./tmp/* &>> mintty_output.txt
+mkdir ./chess-tool-exe  >> mintty_output.txt
+cp `dirname $(which ghc)`/../mingw/bin/libstdc++-6.dll ./chess-tool-exe  &>> mintty_output.txt
+cp `dirname $(which ghc)`/../mingw/bin/libgcc_s_seh-1.dll ./chess-tool-exe  &>> mintty_output.txt
+cp `dirname $(which ghc)`/../mingw/bin/libwinpthread-1.dll ./chess-tool-exe  &>> mintty_output.txt
+cp -r ./.stack-work/dist/*/build/Chess-tool-exe/Chess-tool-exe.exe ./chess-tool-exe  &>> mintty_output.txt
+zip chess-tool-exe.zip ./chess-tool-exe/* &>> mintty_output.txt
 exit
