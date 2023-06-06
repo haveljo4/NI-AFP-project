f66c06eb5ebeb143dc741ed6b4ad2b807c73a217
Josef Havelka <josef.havelka@rapidaddition.com>
2023-06-06 23:04:58 +0200

    added signing of the executables

--- .github/InstallDepsInstallStuff.sh
+++ .github/InstallDepsInstallStuff.sh
@@ -2,6 +2,7 @@ echo "pacman -Syy --noconfirm &>> mintty_output.txt ;" &>> mintty_output.txt
 pacman -Syy --noconfirm &>> mintty_output.txt
 echo "before installing stuff with pacman " &>> mintty_output.txt
 pacman -S --noconfirm git wget tar man gzip zip automake make libtool patch unzip xz bison flex pkg-config &>> mintty_output.txt
+pacman -S --noconfirm mingw-w64-x86_64-osslsigncode &>> mintty_output.txt
 echo "after installing stuff with pacman " &>> mintty_output.txt
 wget http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz
 tar xvfvz autoconf-2.69.tar.gz  &>> ../mintty_output.txt
--- .github/ZipBinaries.sh
+++ .github/ZipBinaries.sh
@@ -3,5 +3,11 @@ cp `dirname $(which ghc)`/../mingw/bin/libstdc++-6.dll ./chess-tool-exe  &>> min
 cp `dirname $(which ghc)`/../mingw/bin/libgcc_s_seh-1.dll ./chess-tool-exe  &>> mintty_output.txt
 cp `dirname $(which ghc)`/../mingw/bin/libwinpthread-1.dll ./chess-tool-exe  &>> mintty_output.txt
 cp -r ./.stack-work/dist/*/build/Chess-tool-exe/Chess-tool-exe.exe ./chess-tool-exe  &>> mintty_output.txt
+openssl genpkey -algorithm RSA -out private.key > /dev/null 2>&1
+openssl x509 -req -days 365 -in csr.csr -signkey private.key -out certificate.pem  &>> mintty_output.txt
+osslsigncode sign -certs certificate.pem -key private.key -in ./chess-tool-exe/Chess-tool-exe.exe -out ./chess-tool-exe/Chess-tool-exe-signed.exe &>> mintty_output.txt
+rm private.key &>> mintty_output.txt
+rm ./chess-tool-exe/Chess-tool-exe.exe  &>> mintty_output.txt
+mv ./chess-tool-exe/Chess-tool-exe-signed.exe ./chess-tool-exe/Chess-tool-exe.exe  &>> mintty_output.txt
 zip chess-tool-exe.zip ./chess-tool-exe/* &>> mintty_output.txt
 exit
