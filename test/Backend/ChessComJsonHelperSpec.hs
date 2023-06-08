7a631559ac77d6e69a021c0be4fef912d31a3b2b
Josef Havelka <josef.havelka@rapidaddition.com>
2023-06-08 21:38:05 +0200

    fixed the tests, removed maintainer line in the package.yml

--- test/Backend/ChessComJsonHelperSpec.hs
+++ test/Backend/ChessComJsonHelperSpec.hs
@@ -10,7 +10,7 @@ spec = do
       JsonHelper.extractYearAndMonthFromURL "https://api.chess.com/pub/player/#&#^(*(/games/2015/12" `shouldBe` Right (2015, 12)  
       JsonHelper.extractYearAndMonthFromURL "https://api.chess.com/pub/player/ mn *& 93 *+/games/2016/-5" `shouldBe` Right (2016, -5)  
     it "filterArchives" $ do
-      JsonHelper.filterArchives 2015 1 2016 6 jsonString  `shouldBe` Right (validUrls, invalidUrls )
+      JsonHelper.filterArchives 2015 1 2016 6 jsonString  `shouldBe` Right (filteredValidUrls, invalidUrls )
         where
           jsonString :: String 
           jsonString =     "{ \"archives\": ["
@@ -20,9 +20,7 @@ spec = do
                            ++ "\"https://api.chess.com/pub/player/somePlayerName/games/2023/06\","
                            ++ "\"mn *& 93 *+/games/2017/13\""
                            ++ "]}"
-          validUrls = ["https://api.chess.com/pub/player/somePlayerName/games/2015/05",
+          filteredValidUrls = ["https://api.chess.com/pub/player/somePlayerName/games/2015/05",
                            "https://api.chess.com/pub/player/somePlayerName/games/2015/06"]
-          invalidUrls = ["https://api.chess.com/pub/player/somePlayerName/games/2009/04",
-                        "https://api.chess.com/pub/player/somePlayerName/games/2023/06",
-                        "mn *& 93 *+/games/2017/13"]
+          invalidUrls = ["mn *& 93 *+/games/2017/13"]
 
